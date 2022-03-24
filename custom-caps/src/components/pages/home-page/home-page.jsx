0cc824fce1b9cfa15bf4b8e7abe6840c56716db5
Syimyk <syimyk310703@gmail.com>
2022-03-25 01:28:25 +0600

    2 blocks

--- custom-caps/src/components/pages/home-page/home-page.jsx
+++ custom-caps/src/components/pages/home-page/home-page.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './style.css';
-
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const HomePage = () => {
+    const caps = useSelector(({caps}) => caps.items)
+    console.log(caps)
     return (
         <div className="wrapper">
             <div className="banner">
-                <div className="container">
                     <div className="main__banner">
                         <div className="main__banner__title">
                             <h2>New Era</h2>
@@ -18,14 +20,18 @@ const HomePage = () => {
                             </p>
                         </div>
                         <div className="main__banner__button">
+                            <Link to='/catalog' >
                             <button className="main__btn">Открыть каталог</button>
+                            </Link>
                         </div>
-                    </div>
-                </div>
+                    </div> 
             </div>
+            
             <div className="main__prew">
                 <div className="prew__blocks">
-                    <div className="prew__block">
+                  {
+                      caps.map((data) => (
+                        <div className="prew__block">
                         <div className="prew__block__img">
                             <img
                                 src="https://www.freepnglogos.com/uploads/cap-png/cap-new-caps-png-download-for-picsart-png-download-nsb-7.png"
@@ -35,13 +41,15 @@ const HomePage = () => {
                         <div className="prew__block__price">
                             <p className="price__yr">2021</p>
 
-                            <p className="price__name">New Era</p>
-                            <div className="price__sub">Houston Rockets</div>
-                            <div className="price__price">2399c</div>
+                            <p className="price__name">{data.brand}</p>
+                            <div className="price__sub">{data.name}</div>
+                            <div className="price__price">{data.price}</div>
                         </div>
                     </div>
+                      ))
+                  }
                 </div>
-            </div>
+            </div> 
         </div>
     );
 };
